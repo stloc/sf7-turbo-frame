86f09426959e25b2524c9797b0535f8658cd09f2
Laurent Guigo <l.guigo@letelegramme.fr>
2024-09-27 10:20:26 +0200

    another action

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\TestType;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,18 +23,29 @@ class DefaultController extends AbstractController
     }
 
     #[Route('/_form', name: 'app_form')]
-    public function form(Request $request): Response
+    public function form(Request $request, LoggerInterface $logger): Response
     {
+        $logger->info('init form');
         $form = $this->createForm(TestType::class);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-
-            return $this->redirectToRoute('app_form');
+            $logger->info('form submitted');
+            return $this->redirectToRoute('app_form_end');
         }
 
         return $this->render('default/form.html.twig', [
             'form' => $form
+        ]);
+    }
+
+    #[Route('/_form_end', name: 'app_form_end')]
+    public function formEnd(LoggerInterface $logger): Response
+    {
+        $logger->info('form end');
+
+        return $this->render('default/form_end.html.twig', [
+
         ]);
     }
 }
