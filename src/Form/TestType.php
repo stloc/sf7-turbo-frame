233f4c0578b5a151bcdd38804afc274d4bfecfa9
Laurent Guigo <l.guigo@letelegramme.fr>
2024-09-27 10:34:31 +0200

    TextType

--- src/Form/TestType.php
+++ src/Form/TestType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,7 @@ class TestType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+            ->add('field_name', TextType::class)
             ->add('save', SubmitType::class)
         ;
     }
